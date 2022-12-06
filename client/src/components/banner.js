b74a900582a12ab70105922a423d0de43a324df5
joseph uzuegbu <josephuzuegbu55@gmail.com>
2022-12-06 14:21:22 +0100

    modified:   client/src/components/banner.js         modified:   client/src/css/banner.css and created   client/src/css/banner.module.css

--- client/src/components/banner.js
+++ client/src/components/banner.js
@@ -3,34 +3,39 @@ import '../css/banner.css';
 import { useReqHttp } from '../customHooks/useReqHttp';
 import Loading from '../admin/components/Loading';
 import { ErrorAlert } from '../admin/components/ErrorAlert';
+import bannerPhoto from '../css/banner.module.css';
 
 export default function Banner() {
   const [alert, setAlert] = useState(true);
   const url = 'setting/change-banner-content';
   const {isLoading, isError, error, data} = useReqHttp(url, null, null);
-  console.log( data.message)
+  // console.log( data)
+ 
 
   if(isLoading){
-    return <Loading>Loading ...</Loading>
+    return <Loading> Loading ... </Loading>
   }
 
   if (isError) {
     return <ErrorAlert alert={alert} setAlert={setAlert}>{error.message}</ErrorAlert>
   }
 
-  if (data.message.length <= 0) {
+  if (data.message.length === 0) {
     return (
-      <div className='container banner_container'>
+      <section className='container'>
+      
+      <div className={ bannerPhoto.banner_container}>
         <div className='banner_content_wrapper'>
             <h4>Velit dolore ut nostrud aliquip pariatur officia dolore.</h4>
             <p className='mt-2'>Sint labore anim qui in.</p>
         </div>
       </div>
+      </section>
     )
   }
 
   return (
-    <div className='container banner_container'>
+    <div className='container' style={{ backgroundImage:`url(${data.message[0].banner_image_link})`,backgroundRepeat:"no-repeat",backgroundSize:"cover" , backgroundPosition:'center'}} height='25em'>
       <div className='banner_content_wrapper'>
         {data.message.map(item=>(
           <Fragment key={item._id}>
