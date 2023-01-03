d953e6f8727d08e5c4233715074b4c7c133c5b99
uzuegbu joseph <josephuzuegbu55@gmail.com>
2023-01-03 15:31:32 +0100

    commit again because of error

---
+++ client/src/admin/components/Edit-mypost-modal.jsx
@@ -0,0 +1,31 @@
+
+
+export function EditMyPostModal(){
+    return(
+        <>
+    {/* Button trigger modal  */}
+        {/* <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editMyPostModal">
+        Launch demo modal
+        </button> */}
+
+       {/* Modal  */}
+        <div className="modal fade" id="editMyPostModal" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div className="modal-dialog">
+            <div className="modal-content">
+            <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+                ...
+            </div>
+            <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+        </div>
+        </>
+    )
+}
--- client/src/admin/pages/Myposts.jsx
+++ client/src/admin/pages/Myposts.jsx
@@ -4,11 +4,15 @@ import MyPostsTable from '../components/myPostsTable';
 import {useReqHttp} from '../../customHooks/useReqHttp';
 import { useContext } from 'react';
 import {AuthContext} from  '../../Context/AuthContext';
-import {useState, useCallback} from 'react';
-
+import {useState} from 'react';
+import Loading from '../components/Loading'
+import {ErrorAlert} from '../components/ErrorAlert'
+;
 export default function Myposts() {
 
     const [page, setPage]= useState(0);
+    const [errorAlert, setErrorAlert]= useState(true);
+
 
     const styleBanner = {
         objectFit:'cover',
@@ -21,14 +25,14 @@ export default function Myposts() {
          return setPage((old)=>old + 1)
     }
 
-     const decreasePages = ()=>{
+     const decreasePage = ()=>{
         return setPage((old)=>old - 1)
     }
    
 
     const {isLoading, isError, error, data,  isFetching, isPreviousData} = useReqHttp('blog/post/currentUser', null, page, isAuthUser, true);
     // console.log(data?.data)
-    console.log(page)
+    
 
   return (
     <div className='container mt-4'>
@@ -67,17 +71,13 @@ export default function Myposts() {
             </div>
 
         </section>
-
-          {/* <section> */}
-            {/* {isLoading&&<>Loading ...</>}
-            {isError&&<>{error.message}</>} */}
             {isLoading?
-            <> Loading ...</>
+            <> <Loading/></>
             :isError?
-            <>{error.message}</>:
+            <ErrorAlert alert={errorAlert} setAlert={setErrorAlert}>{error.message}</ErrorAlert>:
        
             <section className='table_wrapper'>
-                <MyPostsTable currentUserPosts={data.data} increasePage = {increasePage} isFetching={isFetching} isPreviousData={isPreviousData} currentPage = {page} decreasePages = {decreasePages}/> 
+                <MyPostsTable currentUserPosts={data.data} increasePage = {increasePage} isFetching={isFetching} isPreviousData={isPreviousData} currentPage = {page} decreasePage = {decreasePage}/> 
             </section>
             }
         
