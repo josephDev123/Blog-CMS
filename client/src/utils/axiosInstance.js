4b34c6eac223b043d727b87a855a88a62814360b
unknown <josephuzuegbu55@gmail.com>
2023-05-14 15:07:45 +0100

    edited the axios instance

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
  const axiosInstance = axios.create({
-    baseURL: 'https://blog-cms-server.onrender.com:7000/'
+    baseURL: 'https://blog-cms-server.onrender.com/'
 })
 
 // http://localhost:7000
